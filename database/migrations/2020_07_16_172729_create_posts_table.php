c152b77bf2fb8727d44f1fae90c283bffe6c5a77
koudai <61653253+koudai0619@users.noreply.github.com>
2020-07-17 02:50:13 +0900

    ログイン機能の追加

---
+++ database/migrations/2020_07_16_172729_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('body');
+            $table->foreign('user_id')->references('id')->on('users'); //usersテーブルのidに対応してuser_idが外部キーとなる
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
